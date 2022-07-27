239dfd652adce08258fb0efa87bec58a664f94a6
Souleymane DIALLO <s.diallo@macway.com>
2022-07-27 15:03:56 +0200

    show home view display all recipes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main class="container mt-5">
+    <ul class="row p-0">
+      <li class="col col-sm-6 col-md-4" v-for="recipe in recipes" :key="recipe.id">
+        <RouterLink :to="'/recipe/'+ recipe.id">
+          <RecipeCardVue
+            :recipe="{
+              photo: recipe.photo,
+              titre: recipe.titre,
+              personnes: recipe.personnes,
+              niveau: recipe.niveau,
+              tempsPreparation: recipe.tempsPreparation
+            }"
+          />
+          <ModalConfirmDeleteVue :idRecipe="recipe.id" />  
+       </RouterLink>
+      </li>
+    </ul> 
+  </main>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import RecipeCardVue from '@/components/RecipeCard.vue'
+import ModalConfirmDeleteVue from '../components/ModalConfirmDelete.vue';
+import { CantinaGeekApi } from '../services/CantinaGeekAPI';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components: { RecipeCardVue, ModalConfirmDeleteVue },
+  data() {
+    return {
+      recipes: [],
+    };
+  },
+
+  mounted() {
+    CantinaGeekApi.getAllRecipes().then((recipes) => {
+      this.recipes = recipes;
+    });
   }
 }
 </script>
+<style scoped>
+ ul {
+  list-style: none;
+ }
+ ul li a {
+	text-decoration: none;
+  }
+</style>
